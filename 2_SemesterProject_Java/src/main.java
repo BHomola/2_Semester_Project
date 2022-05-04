31a683b09864cb51885ff829f918a1b2bc510fad
Marek <marekk.gabrikk@gmail.com>
2022-05-04 14:58:21 +0200

    dclass

--- 2_SemesterProject_Java/src/main.java
+++ 2_SemesterProject_Java/src/main.java
@@ -4,8 +4,7 @@ import model.Stone;
 public class main {
 
 	public static void main(String[] args) {
-		Stone stone = new Stone(Material.Quartz.Ceniza);
-		System.out.println(stone.getStoneType());
+		
 	}
 
 }
