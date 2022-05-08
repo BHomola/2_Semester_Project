69722c5cbb6dac43f56156693adcaa9b4b48ad62
BHomola <93515506+BHomola@users.noreply.github.com>
2022-05-08 09:33:35 +0200

    IStoneDAO + StoneDAO

--- 2_SemesterProject_Java/src/dataaccess/IStoneDAO.java
+++ 2_SemesterProject_Java/src/dataaccess/IStoneDAO.java
@@ -1,5 +1,17 @@
 package dataaccess;
 
-public interface IStoneDAO {
+import java.util.ArrayList;
 
+import model.IStoneUnit;
+import model.Material;
+import model.Type;
+
+public interface IStoneDAO {
+	public abstract ArrayList<IStoneUnit> getAll();
+	public abstract IStoneUnit getByID();
+	public abstract ArrayList<IStoneUnit> getByMaterial(Material material);
+	public abstract ArrayList<IStoneUnit> getByType(Type type);
+	public abstract int createStone(IStoneUnit stone);
+	public abstract boolean updateStoneUnit(IStoneUnit stone);
+	public abstract boolean deleteStone(IStoneUnit stone);
 }
--- 2_SemesterProject_Java/src/dataaccess/StoneDAO.java
+++ 2_SemesterProject_Java/src/dataaccess/StoneDAO.java
@@ -1,5 +1,55 @@
 package dataaccess;
 
-public class StoneDAO {
+import java.util.ArrayList;
+
+import model.IStoneUnit;
+import model.Material;
+import model.Type;
+
+public class StoneDAO implements IStoneDAO{
+
+	
+	//TODO: Finish when the database is implemented.
+	@Override
+	public ArrayList<IStoneUnit> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IStoneUnit getByID() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<IStoneUnit> getByMaterial(Material material) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<IStoneUnit> getByType(Type type) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int createStone(IStoneUnit stone) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean updateStoneUnit(IStoneUnit stone) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean deleteStone(IStoneUnit stone) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
