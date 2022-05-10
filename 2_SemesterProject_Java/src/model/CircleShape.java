69668f61a3881e45f987ad3578303803c3fd5610
Lavja <93514631+Lavja@users.noreply.github.com>
2022-05-10 12:52:35 +0200

    Ellipse and Circle

--- 2_SemesterProject_Java/src/model/CircleShape.java
+++ 2_SemesterProject_Java/src/model/CircleShape.java
@@ -2,4 +2,19 @@ package model;
 
 public class CircleShape {
 
+	private double diameter;
+
+	public CircleShape(double diameter) {
+		super();
+		this.diameter = diameter;
+	}
+
+	public double getDiameter() {
+		return diameter;
+	}
+
+	public void setDiameter(double diameter) {
+		this.diameter = diameter;
+	}
+	
 }
--- 2_SemesterProject_Java/src/model/ElipseShape.java
+++ 2_SemesterProject_Java/src/model/ElipseShape.java
@@ -2,4 +2,29 @@ package model;
 
 public class ElipseShape {
 
+	private double diameterX;
+	private double diameterY;
+	
+	public ElipseShape(double diameterX, double diameterY) {
+		super();
+		this.diameterX = diameterX;
+		this.diameterY = diameterY;
+	}
+	
+	public double getDiameterX() {
+		return diameterX;
+	}
+	
+	public void setDiameterX(double diameterX) {
+		this.diameterX = diameterX;
+	}
+	
+	public double getDiameterY() {
+		return diameterY;
+	}
+	
+	public void setDiameterY(double diameterY) {
+		this.diameterY = diameterY;
+	}
+	
 }
