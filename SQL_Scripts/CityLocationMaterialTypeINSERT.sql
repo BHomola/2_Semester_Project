2d1772cb34b8b2a86a2ec6b8105ceabf1830bb9c
96chip <catalin.gherciu.11@gmail.com>
2022-05-11 17:08:20 +0200

    InsertScripts

---
+++ SQL_Scripts/CityLocationMaterialTypeINSERT.sql
@@ -0,0 +1,265 @@
+USE [CSC-CSD-S212_10414384]
+GO
+SET IDENTITY_INSERT [dbo].[Type] ON 
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (1, 'Absolute Black', ' ', '\Images\Granite\Absolute-black.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (2, 'Baltic Brown', ' ', '\Images\Granite\Baltic-Brown.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (3, 'Black Fusion', ' ', '\Images\Granite\Black-Fusion.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (4, 'Black Galaxy', ' ', '\Images\Granite\Black-Galaxy.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (5, 'Blue Pearl', ' ', '\Images\Granite\Blue-Pearl.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (6, 'Cardinal Bej', ' ', '\Images\Granite\Cardinal-Bej.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (7, 'Cosmic Black', ' ', '\Images\Granite\Cosmic-Black.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (8, 'Delicatus Gold', ' ', '\Images\Granite\Delicatus-Gold.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (9, 'Extra Blue Volga', ' ', '\Images\Granite\Extra-Blue-Volga.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (10, 'Giallo Sicilia', ' ', '\Images\Granite\Giallo-Sicilia.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (11, 'Giallo-Veneziano', ' ', '\Images\Granite\Giallo-Veneziano.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (12, 'Gialo California', ' ', '\Images\Granite\Gialo-California.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (13, 'Green Butterfly', ' ', '\Images\Granite\Green-Butterfly.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (14, 'Green Oliva', ' ', '\Images\Granite\Green-Oliva.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (15, 'Imperial Red', ' ', '\Images\Granite\Imperial-Red.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (16, 'Kashmir Gold', ' ', '\Images\Granite\Kashmir-Gold.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (17, 'Magma', ' ', '\Images\Granite\Magma.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (18, 'Matrix', ' ', '\Images\Granite\Matrix.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (19, 'Metallicus', ' ', '\Images\Granite\Metallicus.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (20, 'Montblanc', ' ', ' \Images\Granite\Montblanc.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (21, 'Notre Dame', ' ', '\Images\Granite\Notre-Dame.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (22, 'Ouro Brasil', ' ', '\Images\Granite\Ouro-Brasil.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (23, 'Paradiso', ' ', '\Images\Granite\Paradiso.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (24, 'Red Maple-Tree', ' ', '\Images\Granite\Red-Maple-Tree.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (25, 'Rosso Porrino', ' ', '\Images\Granite\Rosso-Porrino.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (26, 'Rosso Santiago', ' ', '\Images\Granite\Rosso-Santiago.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (27, 'Shivakashi', ' ', '\Images\Granite\Shivakashi.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (28, 'Splendor White', ' ', '\Images\Granite\Splendor-White.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (29, 'Tan Brown', ' ', '\Images\Granite\Tan-Brown.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (30, 'Ukraine Grey', ' ', '\Images\Granite\Ukraine-Grey.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (31, 'Ukraines star', ' ', '\Images\Granite\Ukraines-star.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (32, 'Ukranian autumn', ' ', '\Images\Granite\Ukranian-autumn.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (33, 'Green Matrix', ' ', '\Images\Granite\Green-Matrix.jpg', 1)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (34, 'Arabescato', ' ', '\Images\Marble\Arabescato.jpg', 2)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (35, 'Bidasar Gold', ' ', '\Images\Marble\Bidasar-Gold.jpg', 2)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (36, 'Black Fossil', ' ', '\Images\Marble\Black-Fossil.jpg', 2)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (37, 'Botticino Classico', ' ', '\Images\Marble\Botticino-Classico.jpg', 2)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (38, 'Carrara', ' ', '\Images\Marble\Carrara.jpg', 2)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (39, 'Crema Marfil', ' ', '\Images\Marble\Crema-Marfil.jpg', 2)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (40, 'Crema Nova', ' ', '\Images\Marble\Crema-Nova.jpg', 2)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (41, 'Diano Reale', ' ', '\Images\Marble\Diano-Reale.jpg', 2)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (42, 'Emperador Dark', ' ', '\Images\Marble\Emperador-Dark.jpg', 2)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (43, 'Emperador Gold', ' ', '\Images\Marble\Emperador-gold.jpg', 2)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (44, 'Eramosa Brown', ' ', '\Images\Marble\Eramosa-Brown.jpg', 2)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (45, 'Eramosa Silver', ' ', '\Images\Marble\Eramosa-Silver.jpg', 2)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (46, 'Kyknos', ' ', '\Images\Marble\Kyknos.jpg', 2)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (47, 'Lilac', ' ', '\Images\Marble\Lilac.jpg', 2)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (48, 'Marfil', ' ', '\Images\Marble\Marfil.jpg', 2)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (49, 'Misty White', ' ', '\Images\Marble\Misty-White.jpg', 2)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (50, 'Mugla White', ' ', '\Images\Marble\Mugla-White.jpg', 2)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (51, 'Nero Marquina', ' ', '\Images\Marble\Nero-Marquina.jpg', 2)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (52, 'Olympys Beige', ' ', '\Images\Marble\Olympys-Beige.jpg', 2)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (53, 'Rosalia', ' ', '\Images\Marble\Rosalia.jpg', 2)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (54, 'Rosso Levanto', ' ', '\Images\Marble\Rosso-Levanto.jpg', 2)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (55, 'Sivic', ' ', '\Images\Marble\Sivic.jpg', 2)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (56, 'Thassos', ' ', '\Images\Marble\Thassos.jpg', 2)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (57, 'Green Guatemala', ' ', '\Images\Marble\Green-Guatemala.jpg', 2)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (58, 'Afion', ' ', '\Images\Marble-Bio\Afion.jpg', 3)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (59, 'Albufera', ' ', '\Images\Marble-Bio\Albufera.jpg', 3)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (60, 'Anthracite', ' ', '\Images\Marble-Bio\Anthracite.jpg', 3)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (61, 'Beige Faraya', ' ', '\Images\Marble-Bio\Beige-Faraya.jpg', 3)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (62, 'Bering', ' ', '\Images\Marble-Bio\Bering.jpg', 3)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (63, 'Blanco Aura', ' ', '\Images\Marble-Bio\Blanco-Aura.jpg', 3)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (64, 'Blanco Lhasa', ' ', '\Images\Marble-Bio\Blanco-Lhasa.jpg', 3)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (65, 'Caramelo', ' ', '\Images\Marble-Bio\Caramelo.jpg', 3)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (66, 'Crema Valencia', ' ', '\Images\Marble-Bio\Crema-Valencia.jpg', 3)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (67, 'Foggy Nubia', ' ', '\Images\Marble-Bio\Foggy-Nubia.jpg', 3)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (68, 'Jura', ' ', '\Images\Marble-Bio\Jura.jpg', 3)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (69, 'La Perla', ' ', '\Images\Marble-Bio\La-Perla.jpg', 3)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (70, 'Nacarado', ' ', '\Images\Marble-Bio\Nacarado.jpg', 3)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (71, 'Sunset', ' ', '\Images\Marble-Bio\Sunset.jpg', 3)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (72, 'White Faraya', ' ', '\Images\Marble-Bio\White-Faraya.jpg', 3)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (73, 'Aizano', ' ', ' \Images\Quartz-Belenco\Aizano.jpg', 4)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (74, 'Babilon', ' ', '\Images\Quartz-Belenco\Babilon.jpg', 4)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (75, 'Calacatta Venatino', ' ', '\Images\Quartz-Belenco\Calacatta-Venatino.jpg', 4)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (76, 'Chakra Beige', ' ', '\Images\Quartz-Belenco\Chakra-Beige.jpg', 4)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (77, 'Daphne Crack', ' ', '\Images\Quartz-Belenco\Daphne-Crack.jpg', 4)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (78, 'Gala Black', ' ', '\Images\Quartz-Belenco\Gala-Black.jpg', 4)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (79, 'Granada', ' ', '\Images\Quartz-Belenco\Granada.jpg', 4)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (80, 'Iceberg', ' ', '\Images\Quartz-Belenco\Iceberg.jpg', 4)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (81, 'Intestellar', ' ', '\Images\Quartz-Belenco\Intestellar.jpg', 4)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (82, 'Kashmera White', ' ', '\Images\Quartz-Belenco\Kashmera-White.jpg', 4)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (83, 'Kristella White', ' ', '\Images\Quartz-Belenco\Kristella-White.jpg', 4)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (84, 'La Luna', ' ', '\Images\Quartz-Belenco\La-Luna.jpg', 4)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (85, 'Lumiere', ' ', '\Images\Quartz-Belenco\Lumiere.jpg', 4)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (86, 'Montana', ' ', '\Images\Quartz-Belenco\Montana.jpg', 4)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (87, 'Parma', ' ', '\Images\Quartz-Belenco\Parma.jpg', 4)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (88, 'Sahara Beige', ' ', '\Images\Quartz-Belenco\Sahara-Beige.jpg', 4)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (89, 'Spa Black', ' ', '\Images\Quartz-Belenco\Spa-Black.jpg', 4)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (90, 'Teos', ' ', '\Images\Quartz-Belenco\Teos.jpg', 4)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (91, 'Absolut Blanc', ' ', '\Images\Quartz-Compac\Absolut-Blanc.jpg', 5)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (92, 'Ama White', ' ', '\Images\Quartz-Compac\Ama-White.jpg', 5)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (93, 'Antico Rover', ' ', '\Images\Quartz-Compac\Antico-Rover.jpg', 5)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (94, 'Arena', ' ', '\Images\Quartz-Compac\Arena.jpg', 5)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (95, 'Azabache', ' ', '\Images\Quartz-Compac\Azabache.jpg', 5)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (96, 'Black Ama', ' ', '\Images\Quartz-Compac\Black-Ama.jpg', 5)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (97, 'Botticino', ' ', '\Images\Quartz-Compac\Botticino.jpg', 5)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (98, 'Brown Ama', ' ', '\Images\Quartz-Compac\Brown-Ama.jpg', 5)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (99, 'Carrara', ' ', '\Images\Quartz-Compac\Carrara.jpg', 5)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (100, 'Ceniza', ' ', '\Images\Quartz-Compac\Ceniza.jpg',5)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (101, 'Cool Gray', ' ', '\Images\Quartz-Compac\Cool-Gray.jpg', 5)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (102, 'Crystal Beach', ' ', '\Images\Quartz-Compac\Crystal-Beach.jpg', 5)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (103, 'Gray Zement', ' ', '\Images\Quartz-Compac\Gray-Zement.jpg',5)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (104, 'Ice Black Genesis', ' ', '\Images\Quartz-Compac\Ice-Black-Genesis.jpg', 5)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (105, 'Ice Zement', ' ', '\Images\Quartz-Compac\Ice-Zement.jpg', 5)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (106, 'Luna', ' ', '\Images\Quartz-Compac\Luna.jpg', 5)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (107, 'Moka', ' ', '\Images\Quartz-Compac\Moka.jpg', 5)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (108, 'Moon', ' ', '\Images\Quartz-Compac\Moon.jpg', 5)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (109, 'Nocturno', ' ', '\Images\Quartz-Compac\Nocturno.jpg', 5)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (110, 'Smoke Gray', ' ', '\Images\Quartz-Compac\Smoke-Gray.jpg', 5)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (111, 'Snow', ' ', '\Images\Quartz-Compac\Snow.jpg', 5)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (112, 'Unique Calacatta', ' ', '\Images\Quartz-Compac\Unique-Calacatta.jpg',5)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (113, 'Volcano Black', ' ', '\Images\Quartz-Compac\Volcano-Black.jpg', 5)
+GO
+INSERT [dbo].[Type] ([TypeID], [Name], [Description], [Picture], [MaterialID]) VALUES (114, 'Warm Gray', ' ', '\Images\Quartz-Compac\Warm-Gray.jpg', 5)
+GO
+SET IDENTITY_INSERT [dbo].[Type] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Material] ON 
+GO
+INSERT [dbo].[Material] ([MaterialID], [Name], [Description]) VALUES (1, 'Granite','Intrusive igneous rock composed mostly of quartz, alkali feldspar, and plagioclase.')
+GO
+INSERT [dbo].[Material] ([MaterialID], [Name], [Description]) VALUES (2, 'Marble','Metamorphic rock composed of recrystallized carbonate minerals, most commonly calcite or dolomite.')
+GO
+INSERT [dbo].[Material] ([MaterialID], [Name], [Description]) VALUES (3, 'Marble Bio','Bio marble')
+GO
+INSERT [dbo].[Material] ([MaterialID], [Name], [Description]) VALUES (4, 'Quartz Belenco','Hard, crystalline mineral composed of silica (silicon dioxide).')
+GO
+INSERT [dbo].[Material] ([MaterialID], [Name], [Description]) VALUES (5, 'Quartz Compac','High resistance against heat, scratches, abrasion, impact, humidity, stain and it also lacks water absorption.')
+GO
+SET IDENTITY_INSERT [dbo].[Material] OFF
+GO
+SET IDENTITY_INSERT [dbo].[City] ON 
+GO
+INSERT [dbo].[City] ([CityID], [Zipcode], [CityName], [Country]) VALUES (1, 'MD-2000', 'Chisinau', 'Republic of Moldova')
+GO
+INSERT [dbo].[City] ([CityID], [Zipcode], [CityName], [Country]) VALUES (2, 'MD-2089', 'Fauresti', 'Republic of Moldova')
+GO
+SET IDENTITY_INSERT [dbo].[City] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Location] ON 
+GO
+INSERT [dbo].[Location] ([LocationID], [LocationName], [Address], [CityID]) VALUES (1, 'Primary Factory', 'Fauresti', 2)
+GO
+INSERT [dbo].[Location] ([LocationID], [LocationName], [Address], [CityID]) VALUES (2, 'Managers office', 'Ismail 52 ', 1)
+GO
+INSERT [dbo].[Location] ([LocationID], [LocationName], [Address], [CityID]) VALUES (3, 'Sales office', 'Pietrariei 54', 1)
+GO
+SET IDENTITY_INSERT [dbo].[Product] OFF
