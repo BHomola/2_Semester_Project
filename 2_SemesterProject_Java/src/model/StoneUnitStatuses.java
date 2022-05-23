c387f19a274ec553c44ce49195fb32f932dcc1f4
BHomola <93515506+BHomola@users.noreply.github.com>
2022-05-23 10:09:34 +0200

    Update StoneUnitStatuses.java

--- 2_SemesterProject_Java/src/model/StoneUnitStatuses.java
+++ 2_SemesterProject_Java/src/model/StoneUnitStatuses.java
@@ -14,6 +14,19 @@ public enum StoneUnitStatuses {
 	public int getID() {
 		return id;
 	}
+	public static StoneUnitStatuses GetStatusByID(int id) {
+		switch(id) {
+		case 0:
+			return StoneUnitStatuses.WIP;
+		case 1:
+			return StoneUnitStatuses.AVAILABLE;
+		case 2:
+			return StoneUnitStatuses.UNAVAILABLE;
+		case 3:
+			return StoneUnitStatuses.OTHER;
+		}
+		return StoneUnitStatuses.OTHER;
+	}
 
 	public String toString() {
 		return statusString;
