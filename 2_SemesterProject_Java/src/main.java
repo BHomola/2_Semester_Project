8e14ea1a6714176a213095c9ea134537b2253b47
Marek <marekk.gabrikk@gmail.com>
2022-05-24 14:51:56 +0200

    Update main.java

--- 2_SemesterProject_Java/src/main.java
+++ 2_SemesterProject_Java/src/main.java
@@ -1,5 +1,6 @@
 import java.sql.SQLException;
 
+import dataaccess.DBConnection;
 import dataaccess.OrderDAO;
 
 public class main {
@@ -7,15 +8,15 @@ public class main {
 	public static void main(String[] args) {
 		OrderDAO oDAO = new OrderDAO();
 		try {
-			
 			//starting thread that checks for database connection
 			DatabaseCheckThread thread = new DatabaseCheckThread();
 			thread.start();
+			thread.join();
+
+			System.out.println(oDAO.getOrdersByCustomerID(50));
+			System.out.println(oDAO.getByID(2));
 			
-			
-			System.out.println(oDAO.getAll());
-			//DBConnection.getConnection();
-		} catch (SQLException e) {
+		} catch (SQLException | InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
