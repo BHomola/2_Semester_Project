7444794ee50c47a4ac445b540f36e3bf6b976be4
96chip <catalin.gherciu.11@gmail.com>
2022-05-25 10:01:36 +0200

    TestJSONShapeDAO

---
+++ 2_SemesterProject_Java/src/test/TestJSONShapeDAO.java
@@ -0,0 +1,45 @@
+package test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import dataaccess.JSONShapeDAO;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.sql.SQLException;
+
+import model.CircleShape;
+
+class TestJSONShapeDAO {
+	CircleShape cShape;
+	JSONShapeDAO jsonDAO;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		jsonDAO = new JSONShapeDAO();
+		cShape = new CircleShape("2Circle", 4, 5.00);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+/*	@Test
+	void testCreateShape() throws SQLException {
+		equals(jsonDAO.createShape(cShape, 1));
+	} */
+	
+	@Test
+	void testUpdateShape() throws SQLException {
+		assertTrue(jsonDAO.updateShape(cShape));
+	}
+	
+	@Test
+	void testDeleteShape() throws SQLException {
+		assertTrue(jsonDAO.deleteShape(cShape));
+	}
+
+}
